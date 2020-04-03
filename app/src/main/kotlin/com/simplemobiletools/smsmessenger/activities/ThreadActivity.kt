f938c14cbae77676ee8e47593b0b9ac4552ee965
tibbi <tibor@kaputa.sk>
2020-04-03 19:51:23 +0200

    show the appropriate messages at opening a thread

--- app/src/main/kotlin/com/simplemobiletools/smsmessenger/activities/ThreadActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/activities/ThreadActivity.kt
@@ -2,8 +2,10 @@ package com.simplemobiletools.smsmessenger.activities
 
 import android.os.Bundle
 import com.simplemobiletools.smsmessenger.R
+import com.simplemobiletools.smsmessenger.adapters.ThreadAdapter
 import com.simplemobiletools.smsmessenger.extensions.getMessages
 import com.simplemobiletools.smsmessenger.helpers.THREAD_ID
+import kotlinx.android.synthetic.main.activity_thread.*
 
 class ThreadActivity : SimpleActivity() {
 
@@ -12,5 +14,12 @@ class ThreadActivity : SimpleActivity() {
         setContentView(R.layout.activity_thread)
         val threadID = intent.getIntExtra(THREAD_ID, 0)
         val messages = getMessages(threadID)
+        messages.sortBy { it.id }
+
+        ThreadAdapter(this, messages, thread_messages_list, thread_messages_fastscroller) {
+
+        }.apply {
+            thread_messages_list.adapter = this
+        }
     }
 }
