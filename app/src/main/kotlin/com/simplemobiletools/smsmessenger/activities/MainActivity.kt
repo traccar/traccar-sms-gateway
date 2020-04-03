feb4cd6e9ac893264da17077bbcad17e1cb14371
tibbi <tibor@kaputa.sk>
2020-04-03 10:12:58 +0200

    uploading the initial project files

---
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.simplemobiletools.smsmessenger.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.simplemobiletools.smsmessenger.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
