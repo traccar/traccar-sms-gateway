3373ce66e154c0dc32cd2aa32c9d40085a9a6dee
tibbi <tibor@kaputa.sk>
2020-04-05 17:16:48 +0200

    add a contact searching field at the New Message activity

--- app/src/main/kotlin/com/simplemobiletools/smsmessenger/activities/NewMessageActivity.kt
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/activities/NewMessageActivity.kt
@@ -1,24 +1,19 @@
 package com.simplemobiletools.smsmessenger.activities
 
 import android.os.Bundle
-import com.simplemobiletools.commons.extensions.applyColorFilter
-import com.simplemobiletools.commons.extensions.onTextChangeListener
-import com.simplemobiletools.commons.extensions.value
+import android.view.WindowManager
+import com.simplemobiletools.commons.extensions.updateTextColors
 import com.simplemobiletools.smsmessenger.R
-import com.simplemobiletools.smsmessenger.adapters.ThreadAdapter
-import com.simplemobiletools.smsmessenger.extensions.config
-import com.simplemobiletools.smsmessenger.extensions.getMessages
-import com.simplemobiletools.smsmessenger.helpers.THREAD_ID
-import com.simplemobiletools.smsmessenger.helpers.THREAD_NAME
-import com.simplemobiletools.smsmessenger.helpers.THREAD_NUMBER
-import com.simplemobiletools.smsmessenger.models.ThreadDateTime
-import com.simplemobiletools.smsmessenger.models.ThreadItem
-import kotlinx.android.synthetic.main.activity_thread.*
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : SimpleActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
         title = getString(R.string.create_new_message)
+        updateTextColors(new_message_holder)
+
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
+        new_message_to.requestFocus()
     }
 }
