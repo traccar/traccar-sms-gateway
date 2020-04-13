e828381c9a9b5d34e3a6650f22487b6847ef1e91
tibbi <tibor@kaputa.sk>
2020-04-13 22:24:23 +0200

    lets not use the ARCHIVED Threads field, not all devices have it

--- app/src/main/kotlin/com/simplemobiletools/smsmessenger/extensions/Context.kt
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/extensions/Context.kt
@@ -175,8 +175,8 @@ fun Context.getConversations(): ArrayList<Conversation> {
         Threads.RECIPIENT_IDS
     )
 
-    val selection = "${Threads.ARCHIVED} = ? AND ${Threads.MESSAGE_COUNT} > ?"
-    val selectionArgs = arrayOf("0", "0")
+    val selection = "${Threads.MESSAGE_COUNT} > ?"
+    val selectionArgs = arrayOf("0")
     val conversations = ArrayList<Conversation>()
     queryCursor(uri, projection, selection, selectionArgs, showErrors = true) { cursor ->
         val id = cursor.getIntValue(Threads._ID)
