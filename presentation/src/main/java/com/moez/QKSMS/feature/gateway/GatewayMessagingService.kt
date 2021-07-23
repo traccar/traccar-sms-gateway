b4075c9e090429de20d99bfe156ba15be779be7d
Anton Tananaev <anton.tananaev@gmail.com>
2021-07-22 20:49:45 -0700

    Toast on main thread

--- presentation/src/main/java/com/moez/QKSMS/feature/gateway/GatewayMessagingService.kt
+++ presentation/src/main/java/com/moez/QKSMS/feature/gateway/GatewayMessagingService.kt
@@ -1,6 +1,8 @@
 package com.moez.QKSMS.feature.gateway
 
 import android.annotation.SuppressLint
+import android.os.Handler
+import android.os.Looper
 import android.telephony.SmsManager
 import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -9,6 +11,8 @@ import com.google.firebase.messaging.RemoteMessage
 @SuppressLint("MissingFirebaseInstanceTokenRefresh")
 class GatewayMessagingService : FirebaseMessagingService() {
 
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         val phone = remoteMessage.data["phone"]
         val message = remoteMessage.data["message"]
@@ -16,7 +20,9 @@ class GatewayMessagingService : FirebaseMessagingService() {
             try {
                 SmsManager.getDefault().sendTextMessage(phone, null, message, null, null)
             } catch (e: Exception) {
-                Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+                handler.post {
+                    Toast.makeText(this, e.message, Toast.LENGTH_LONG).show()
+                }
             }
         }
     }
