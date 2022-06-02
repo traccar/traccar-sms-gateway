7e60d25a079d8d82ec3e89aed86d2b24092d6e31
tibbi <tibor@kaputa.sk>
2022-06-02 23:15:17 +0200

    lower the loaded messages count to 50

--- app/src/main/kotlin/com/simplemobiletools/smsmessenger/helpers/Constants.kt
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/helpers/Constants.kt
@@ -54,7 +54,7 @@ const val FILE_SIZE_600_KB = 614_400L
 const val FILE_SIZE_1_MB = 1_048_576L
 const val FILE_SIZE_2_MB = 2_097_152L
 
-const val MESSAGES_LIMIT = 100
+const val MESSAGES_LIMIT = 50
 
 fun refreshMessages() {
     EventBus.getDefault().post(Events.RefreshMessages())
