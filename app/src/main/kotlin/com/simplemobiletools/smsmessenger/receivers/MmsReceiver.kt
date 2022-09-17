cb9e91ce085908e06cca89ebfc5ff5137527146e
Naveen <snaveen935@gmail.com>
2022-09-17 23:52:06 +0530

    Add the ability to block numbers by pattern

--- app/src/main/kotlin/com/simplemobiletools/smsmessenger/receivers/MmsReceiver.kt
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/receivers/MmsReceiver.kt
@@ -6,18 +6,22 @@ import android.os.Handler
 import android.os.Looper
 import com.bumptech.glide.Glide
 import com.simplemobiletools.commons.extensions.isNumberBlocked
+import com.simplemobiletools.commons.extensions.normalizePhoneNumber
 import com.simplemobiletools.commons.helpers.ensureBackgroundThread
 import com.simplemobiletools.smsmessenger.R
 import com.simplemobiletools.smsmessenger.extensions.*
 
 // more info at https://github.com/klinker41/android-smsmms
 class MmsReceiver : com.klinker.android.send_message.MmsReceivedReceiver() {
+
+    override fun isAddressBlocked(context: Context, address: String): Boolean {
+        val normalizedAddress = address.normalizePhoneNumber()
+        return context.isNumberBlocked(normalizedAddress)
+    }
+
     override fun onMessageReceived(context: Context, messageUri: Uri) {
         val mms = context.getLatestMMS() ?: return
         val address = mms.participants.firstOrNull()?.phoneNumbers?.first()?.normalizedNumber ?: ""
-        if (context.isNumberBlocked(address)) {
-            return
-        }
 
         val size = context.resources.getDimension(R.dimen.notification_large_icon_size).toInt()
         ensureBackgroundThread {
