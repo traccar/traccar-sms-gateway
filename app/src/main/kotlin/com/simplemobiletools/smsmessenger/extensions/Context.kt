f4575401f59db32e8739261bf5bed5a5be088580
Naveen <snaveen935@gmail.com>
2022-09-18 22:54:51 +0530

    Use the existing extension for api version check

--- app/src/main/kotlin/com/simplemobiletools/smsmessenger/extensions/Context.kt
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/extensions/Context.kt
@@ -16,7 +16,6 @@ import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.RingtoneManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -997,7 +996,7 @@ fun Context.clearAllMessagesIfNeeded() {
 }
 
 fun Context.subscriptionManagerCompat(): SubscriptionManager {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+    return if (isMarshmallowPlus()) {
         getSystemService(SubscriptionManager::class.java)
     } else {
         @Suppress("DEPRECATION")
