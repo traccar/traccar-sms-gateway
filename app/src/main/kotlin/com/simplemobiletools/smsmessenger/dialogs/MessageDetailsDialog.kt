3adfdd401ef43d1e8d262d8860397f589764d941
Ensar Sarajčić <dev@ensarsarajcic.com>
2023-07-11 11:02:55 +0200

    Update dialog to reuse properties dialog from file manager

--- app/src/main/kotlin/com/simplemobiletools/smsmessenger/dialogs/MessageDetailsDialog.kt
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/dialogs/MessageDetailsDialog.kt
@@ -3,48 +3,41 @@ package com.simplemobiletools.smsmessenger.dialogs
 import android.annotation.SuppressLint
 import android.telephony.SubscriptionInfo
 import com.simplemobiletools.commons.activities.BaseSimpleActivity
-import com.simplemobiletools.commons.extensions.getAlertDialogBuilder
-import com.simplemobiletools.commons.extensions.getTimeFormat
-import com.simplemobiletools.commons.extensions.setupDialogStuff
+import com.simplemobiletools.commons.dialogs.BasePropertiesDialog
+import com.simplemobiletools.commons.extensions.*
 import com.simplemobiletools.smsmessenger.R
 import com.simplemobiletools.smsmessenger.extensions.config
 import com.simplemobiletools.smsmessenger.extensions.subscriptionManagerCompat
 import com.simplemobiletools.smsmessenger.models.Message
-import kotlinx.android.synthetic.main.dialog_message_details.view.dialog_message_details_text_value
 import org.joda.time.DateTime
 
-class MessageDetailsDialog(val activity: BaseSimpleActivity, val message: Message) {
+class MessageDetailsDialog(val activity: BaseSimpleActivity, val message: Message) : BasePropertiesDialog(activity) {
     init {
         @SuppressLint("MissingPermission")
         val availableSIMs = activity.subscriptionManagerCompat().activeSubscriptionInfoList
 
-        @SuppressLint("SetTextI18n")
-        val view = activity.layoutInflater.inflate(R.layout.dialog_message_details, null).apply {
-            dialog_message_details_text_value.text = mutableListOf<String>().apply {
-                add("${message.getReceiverOrSenderLabel()}: ${message.getReceiverOrSenderPhoneNumbers()}")
-                if (availableSIMs.count() > 1) {
-                    add("SIM: ${message.getSIM(availableSIMs)}")
-                }
-                add("${message.getSentOrReceivedAtLabel()}: ${message.getSentOrReceivedAt()}")
-            }.joinToString(separator = System.lineSeparator())
+        addProperty(message.getSenderOrReceiverLabel(), message.getSenderOrReceiverPhoneNumbers())
+        if (availableSIMs.count() > 1) {
+            addProperty(R.string.message_details_sim, message.getSIM(availableSIMs))
         }
+        addProperty(message.getSentOrReceivedAtLabel(), message.getSentOrReceivedAt())
 
         activity.getAlertDialogBuilder()
             .setPositiveButton(R.string.ok) { _, _ -> }
             .apply {
-                activity.setupDialogStuff(view, this, R.string.message_details)
+                activity.setupDialogStuff(mDialogView, this, R.string.message_details)
             }
     }
 
-    private fun Message.getReceiverOrSenderLabel(): String {
+    private fun Message.getSenderOrReceiverLabel(): Int {
         return if (isReceivedMessage()) {
-            activity.getString(R.string.message_details_sender)
+            R.string.message_details_sender
         } else {
-            activity.getString(R.string.message_details_receiver)
+            R.string.message_details_receiver
         }
     }
 
-    private fun Message.getReceiverOrSenderPhoneNumbers(): String {
+    private fun Message.getSenderOrReceiverPhoneNumbers(): String {
         return if (isReceivedMessage()) {
             formatContactInfo(senderName, senderPhoneNumber)
         } else {
@@ -66,11 +59,11 @@ class MessageDetailsDialog(val activity: BaseSimpleActivity, val message: Messag
         return availableSIMs.firstOrNull { it.subscriptionId == subscriptionId }?.displayName?.toString() ?: activity.getString(R.string.unknown)
     }
 
-    private fun Message.getSentOrReceivedAtLabel(): String {
+    private fun Message.getSentOrReceivedAtLabel(): Int {
         return if (isReceivedMessage()) {
-            activity.getString(R.string.message_details_received_at)
+            R.string.message_details_received_at
         } else {
-            activity.getString(R.string.message_details_sent_at)
+            R.string.message_details_sent_at
         }
     }
 
