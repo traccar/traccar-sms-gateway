5363af1071adaee9a4a1947b5e62fa8c9250d45c
merkost <konstantinlikes@gmail.com>
2023-07-18 12:38:31 +1000

    Added serialization

--- app/src/main/kotlin/com/simplemobiletools/smsmessenger/models/SmsBackup.kt
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/models/SmsBackup.kt
@@ -5,7 +5,9 @@ import android.content.ContentValues
 import android.provider.Telephony
 import androidx.core.content.contentValuesOf
 import com.google.gson.annotations.SerializedName
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class SmsBackup(
     @SerializedName("sub_id")
     val subscriptionId: Long,
