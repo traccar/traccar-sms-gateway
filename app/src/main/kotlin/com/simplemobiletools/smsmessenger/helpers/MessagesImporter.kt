679236e3fa8c9503bdf5cb5c65fd36e7e4c4e736
merkost <konstantinlikes@gmail.com>
2023-07-18 16:23:46 +1000

    MessagesImporter and MessagesReader refactoring

--- app/src/main/kotlin/com/simplemobiletools/smsmessenger/helpers/MessagesImporter.kt
+++ app/src/main/kotlin/com/simplemobiletools/smsmessenger/helpers/MessagesImporter.kt
@@ -1,102 +1,46 @@
 package com.simplemobiletools.smsmessenger.helpers
 
 import android.content.Context
-import android.util.JsonToken
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 import com.simplemobiletools.commons.extensions.showErrorToast
 import com.simplemobiletools.commons.helpers.ensureBackgroundThread
 import com.simplemobiletools.smsmessenger.extensions.config
-import com.simplemobiletools.smsmessenger.helpers.MessagesImporter.ImportResult.IMPORT_FAIL
-import com.simplemobiletools.smsmessenger.helpers.MessagesImporter.ImportResult.IMPORT_NOTHING_NEW
-import com.simplemobiletools.smsmessenger.helpers.MessagesImporter.ImportResult.IMPORT_OK
-import com.simplemobiletools.smsmessenger.helpers.MessagesImporter.ImportResult.IMPORT_PARTIAL
-import com.simplemobiletools.smsmessenger.models.MmsBackup
-import com.simplemobiletools.smsmessenger.models.SmsBackup
-import java.io.File
+import com.simplemobiletools.smsmessenger.models.*
 
 class MessagesImporter(private val context: Context) {
-    enum class ImportResult {
-        IMPORT_FAIL, IMPORT_OK, IMPORT_PARTIAL, IMPORT_NOTHING_NEW
-    }
 
-    private val gson = Gson()
     private val messageWriter = MessagesWriter(context)
     private val config = context.config
     private var messagesImported = 0
     private var messagesFailed = 0
 
-    fun importMessages(path: String, onProgress: (total: Int, current: Int) -> Unit = { _, _ -> }, callback: (result: ImportResult) -> Unit) {
+    fun importMessages(messagesBackup: List<MessagesBackup>, callback: (result: ImportResult) -> Unit) {
         ensureBackgroundThread {
             try {
-                val inputStream = if (path.contains("/")) {
-                    File(path).inputStream()
-                } else {
-                    context.assets.open(path)
-                }
-
-                inputStream.bufferedReader().use { reader ->
-                    val jsonReader = gson.newJsonReader(reader)
-                    val smsMessageType = object : TypeToken<SmsBackup>() {}.type
-                    val mmsMessageType = object : TypeToken<MmsBackup>() {}.type
-
-                    jsonReader.beginArray()
-                    while (jsonReader.hasNext()) {
-                        jsonReader.beginObject()
-                        while (jsonReader.hasNext()) {
-                            val nextToken = jsonReader.peek()
-                            if (nextToken.ordinal == JsonToken.NAME.ordinal) {
-                                val msgType = jsonReader.nextName()
-
-                                if ((!msgType.equals("sms") && !msgType.equals("mms")) ||
-                                    (msgType.equals("sms") && !config.importSms) ||
-                                    (msgType.equals("mms") && !config.importMms)
-                                ) {
-                                    jsonReader.skipValue()
-                                    continue
-                                }
-
-                                jsonReader.beginArray()
-                                while (jsonReader.hasNext()) {
-                                    try {
-                                        if (msgType.equals("sms")) {
-                                            val message = gson.fromJson<SmsBackup>(jsonReader, smsMessageType)
-                                            messageWriter.writeSmsMessage(message)
-                                        } else {
-                                            val message = gson.fromJson<MmsBackup>(jsonReader, mmsMessageType)
-                                            messageWriter.writeMmsMessage(message)
-                                        }
-
-                                        messagesImported++
-                                    } catch (e: Exception) {
-                                        context.showErrorToast(e)
-                                        messagesFailed++
-
-                                    }
-                                }
-                                jsonReader.endArray()
-                            } else {
-                                jsonReader.skipValue()
-                            }
+                messagesBackup.forEach { message ->
+                    try {
+                        if (message.backupType == BackupType.SMS && config.importSms) {
+                            messageWriter.writeSmsMessage(message as SmsBackup)
+                        } else if (message.backupType == BackupType.MMS && config.importMms) {
+                            messageWriter.writeMmsMessage(message as MmsBackup)
                         }
-
-                        jsonReader.endObject()
-                        refreshMessages()
+                        else return@forEach
+                        messagesImported++
+                    } catch (e: Exception) {
+                        context.showErrorToast(e)
+                        messagesFailed++
                     }
-
-                    jsonReader.endArray()
                 }
+                refreshMessages()
             } catch (e: Exception) {
                 context.showErrorToast(e)
-                messagesFailed++
             }
 
             callback.invoke(
                 when {
-                    messagesImported == 0 && messagesFailed == 0 -> IMPORT_NOTHING_NEW
-                    messagesFailed > 0 && messagesImported > 0 -> IMPORT_PARTIAL
-                    messagesFailed > 0 -> IMPORT_FAIL
-                    else -> IMPORT_OK
+                    messagesImported == 0 && messagesFailed == 0 -> ImportResult.IMPORT_NOTHING_NEW
+                    messagesFailed > 0 && messagesImported > 0 -> ImportResult.IMPORT_PARTIAL
+                    messagesFailed > 0 -> ImportResult.IMPORT_FAIL
+                    else -> ImportResult.IMPORT_OK
                 }
             )
         }
